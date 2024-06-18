0f7261c19482a26d6ae895b4b65b094caccc9281
Ajay Kumar <126579063+AjayKumar0786@users.noreply.github.com>
2024-06-18 23:19:09 +0530

    Add files via upload

---
+++ Backend In Java/Java Projects/Core Java/src/Methods/Calculator1.java
@@ -0,0 +1,30 @@
+package Methods;
+
+public class Calculator1 {
+	int a, b, c;
+	
+	
+	
+	void add() {
+
+		a=10;
+		b = 10;
+		c= a+b;
+		
+		System.out.println(c);
+				
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Calculator1 cal = new Calculator1();
+		
+		cal.add();
+		
+		
+
+	}
+
+}
---
+++ Backend In Java/Java Projects/Core Java/src/Methods/methodoverloading.java
@@ -0,0 +1,30 @@
+package Methods;
+
+public class methodoverloading {
+	
+	void add(int a, int b) {
+		
+		
+	}
+	
+	void add(float a, float b) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[]args) {
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
